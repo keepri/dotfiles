eddcf528b824d39d1517dcbbe7fd49fa62509fcc
Pitul, Ciprian <ciprian.pitul@haufe.com>
2024-01-15 14:44:54 +0200

    feat: netrw list style collapseable

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.mapleader = " ";
 vim.g.maplocalleader = " ";
 
 vim.g.netrw_banner = 0;
+vim.g.netrw_liststyle = 3;
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
