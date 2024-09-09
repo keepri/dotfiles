d4aa3f9f598c03a8ed326df4eba63c204d604668
Pitul, Ciprian <ciprian.pitul@haufe.com>
2024-09-09 09:49:24 +0300

    chore: use ts_ls as server name

--- nvim/init.lua
+++ nvim/init.lua
@@ -193,7 +193,7 @@ local servers = {
             },
         },
     },
-    tsserver = {
+    ts_ls = {
         filetypes = {
             "javascript",
             "javascriptreact",
@@ -233,12 +233,6 @@ mason_lspconfig.setup({
 
 mason_lspconfig.setup_handlers({
     function (server_name)
-        -- TODO TEMP should be removed once ts_ls becomes a valid LSP server
-        -- https://github.com/neovim/nvim-lspconfig/pull/3232
-        if server_name == "tsserver" then
-            server_name = "ts_ls";
-        end;
-
         require("lspconfig")[server_name].setup({
             capabilities = capabilities,
             on_attach = on_attach,
