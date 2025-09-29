e4f4ae5dc4641e92c448fa1de47cfcc34e7cfdf1
Pitul, Ciprian <ciprian.pitul@haufe.com>
2025-09-29 17:23:53 +0300

    fix: jsonls

--- nvim/lsp/jsonls.lua
+++ nvim/lsp/jsonls.lua
@@ -1,5 +1,7 @@
 ---@type vim.lsp.Config
 return {
+    cmd = { "vscode-json-language-server", "--stdio" },
+
     filetypes = {
         "json",
         "jsonc",
@@ -7,28 +9,31 @@ return {
 
     root_markers = { ".git" },
 
-    json = {
-        -- Schemas https://www.schemastore.org
-        schemas = {
-            {
-                fileMatch = { "package.json" },
-                url = "https://json.schemastore.org/package.json",
-            },
-            {
-                fileMatch = { "tsconfig*.json" },
-                url = "https://json.schemastore.org/tsconfig.json",
-            },
-            {
-                fileMatch = {
-                    ".prettierrc",
-                    ".prettierrc.json",
-                    "prettier.config.json",
+    settings = {
+        json = {
+            format = { enable = true },
+            validate = { enable = true },
+            schemas = {
+                {
+                    fileMatch = { "package.json" },
+                    url = "https://json.schemastore.org/package.json",
                 },
-                url = "https://json.schemastore.org/prettierrc.json",
-            },
-            {
-                fileMatch = { ".eslintrc", ".eslintrc.json" },
-                url = "https://json.schemastore.org/eslintrc.json",
+                {
+                    fileMatch = { "tsconfig*.json" },
+                    url = "https://json.schemastore.org/tsconfig.json",
+                },
+                {
+                    fileMatch = {
+                        ".prettierrc",
+                        ".prettierrc.json",
+                        "prettier.config.json",
+                    },
+                    url = "https://json.schemastore.org/prettierrc.json",
+                },
+                {
+                    fileMatch = { ".eslintrc", ".eslintrc.json" },
+                    url = "https://json.schemastore.org/eslintrc.json",
+                },
             },
         },
     },
