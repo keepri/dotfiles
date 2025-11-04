447d923453cb1fcb78bbfa9b8456e7b2c7752161
KIPRI <hi@kipri.dev>
2025-11-04 20:50:13 +0200

    chore: telescope keybinds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -76,101 +76,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
         end;
 
         vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {});
-        vim.keymap.set(
-            "n",
-            "<leader>sG",
-            ":LiveGrepGitRoot<cr>",
-            { desc = "[S]earch by [G]rep on Git Root" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>?",
-            builtin.oldfiles,
-            { desc = "[?] Find recently opened files" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader><space>",
-            builtin.buffers,
-            { desc = "[ ] Find existing buffers" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>/",
-            function ()
-                local dropdown = themes.get_dropdown({
-                    winblend = 10,
-                    previewer = false,
-                });
-                builtin.current_buffer_fuzzy_find(dropdown);
-            end,
-            { desc = "[/] Fuzzily search in current buffer" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>s/",
-            telescope_live_grep_open_files,
-            { desc = "[S]earch [/] in Open Files" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>ss",
-            builtin.builtin,
-            { desc = "[S]earch [S]elect Telescope" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>gf",
-            builtin.git_files,
-            { desc = "Search [G]it [F]iles" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sf",
-            builtin.find_files,
-            { desc = "[S]earch [F]iles" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sh",
-            builtin.help_tags,
-            { desc = "[S]earch [H]elp" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sw",
-            builtin.grep_string,
-            { desc = "[S]earch current [W]ord" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sg",
-            builtin.live_grep,
-            { desc = "[S]earch by [G]rep" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sd",
-            builtin.diagnostics,
-            { desc = "[S]earch [D]iagnostics" }
-        );
-
-        vim.keymap.set(
-            "n",
-            "<leader>sr",
-            builtin.resume,
-            { desc = "[S]earch [R]esume" }
-        );
+        vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" });
+        vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" });
+        vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" });
+        -- vim.keymap.set("n", "<leader>/",
+        --     function ()
+        --         local dropdown = themes.get_dropdown({
+        --             winblend = 10,
+        --             previewer = false,
+        --         });
+        --         builtin.current_buffer_fuzzy_find(dropdown);
+        --     end,
+        --     { desc = "[/] Fuzzily search in current buffer" }
+        -- );
+        vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" });
+        vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" });
+        vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" });
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" });
+        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" });
+        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" });
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" });
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" });
+        vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" });
     end,
 });
