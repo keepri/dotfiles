4eaa0c981878211265fd670452de4e16b54c33e6
KIPRI <hi@kipri.dev>
2025-11-05 20:14:32 +0200

    chore: tmux start script

--- tmux/start.sh
+++ tmux/start.sh
@@ -1,5 +1,7 @@
+# add this to your .zshrc, .bashrc or wherever you desire
 if [ ! -n "$TMUX" ]; then
     if ! tmux a 2>/dev/null; then
-        tmux new -c ~/code -s sessy
+        # -c ~/code
+        tmux new -s sessy
     fi
 fi
